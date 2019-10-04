37600e6d09d6a9bf20450e55b63bfe9a69d858b2
Riley Moss <riley.moss@accenture.com>
2019-10-04 11:58:55 -0500

    Use ViewModel in DeviceScanActivity
    Signed-off-by: Josh Radecki <joshua.a.radecki@accenture.com>

--- app/src/main/java/com/acn/componenthealthmonitor/DeviceScanActivity.java
+++ app/src/main/java/com/acn/componenthealthmonitor/DeviceScanActivity.java
@@ -1,10 +1,14 @@
 package com.acn.componenthealthmonitor;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
 
 public class DeviceScanActivity extends AppCompatActivity {
+
+    private DeviceScanViewModel viewModel;
 
     private BleRecyclerAdapter adapter;
 
@@ -12,6 +16,8 @@ public class DeviceScanActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_device_scan);
+
+        viewModel = ViewModelProviders.of(this).get(DeviceScanViewModel.class);
 
         adapter = new BleRecyclerAdapter();
     }
---
+++ app/src/main/java/com/acn/componenthealthmonitor/DeviceScanViewModel.java
@@ -0,0 +1,7 @@
+package com.acn.componenthealthmonitor;
+
+import androidx.lifecycle.ViewModel;
+
+class DeviceScanViewModel extends ViewModel {
+
+}
