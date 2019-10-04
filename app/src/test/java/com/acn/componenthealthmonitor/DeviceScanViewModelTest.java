b6f756f1f3caf35bdec691d2aaf267bd24fea88e
Riley Moss <riley.moss@accenture.com>
2019-10-04 13:46:47 -0500

    After coarse locationl, prepare enables location
    Signed-off-by: Josh Radecki <joshua.a.radecki@accenture.com>

--- app/src/test/java/com/acn/componenthealthmonitor/DeviceScanViewModelTest.java
+++ app/src/test/java/com/acn/componenthealthmonitor/DeviceScanViewModelTest.java
@@ -4,21 +4,30 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 
-import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(RobolectricTestRunner.class)
 public class DeviceScanViewModelTest {
 
+    private PermissionsHelper mockHelper = mock(PermissionsHelper.class);
+    private DeviceScanViewModel viewModel = new DeviceScanViewModel();
+
     @Test
     public void prepareForScanning_doesNotHaveCoarseLocation() {
-        PermissionsHelper mockHelper = mock(PermissionsHelper.class);
-        DeviceScanViewModel viewModel = new DeviceScanViewModel();
 
         viewModel.prepareForScanning(mockHelper);
 
         verify(mockHelper).requestCoarseLocationPermissions();
+    }
+
+    @Test
+    public void prepareForScanning_doesNotHaveLocationTurnedOn() {
+        when(mockHelper.hasCoarseLocationPermission()).thenReturn(true);
+        viewModel.prepareForScanning(mockHelper);
+
+        verify(mockHelper).enableLocation();
     }
 
 }
