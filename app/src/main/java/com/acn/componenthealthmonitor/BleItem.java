db2a5371de74a9af69d685c7bcf8ee4a4726387b
Riley Moss <riley.moss@accenture.com>
2019-10-04 12:57:53 -0500

    Data binding to list item
    Signed-off-by: Josh Radecki <joshua.a.radecki@accenture.com>

--- app/src/main/java/com/acn/componenthealthmonitor/BleItem.java
+++ app/src/main/java/com/acn/componenthealthmonitor/BleItem.java
@@ -1,6 +1,18 @@
 package com.acn.componenthealthmonitor;
 
+import android.bluetooth.BluetoothDevice;
+
 public class BleItem {
 
+    private BluetoothDevice device;
+    private String name;
+
+    public BluetoothDevice getDevice() {
+        return device;
+    }
+
+    public String getName() {
+        return name;
+    }
 
 }
