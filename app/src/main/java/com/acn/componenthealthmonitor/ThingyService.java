f534ec3afa847268ebf741ec89b68b5e3723f2e7
Riley Moss <riley.moss@accenture.com>
2019-10-04 16:30:49 -0500

    Updated ThingyService with notifications
    Signed-off-by: Josh Radecki <joshua.a.radecki@accenture.com>

--- app/src/main/java/com/acn/componenthealthmonitor/ThingyService.java
+++ app/src/main/java/com/acn/componenthealthmonitor/ThingyService.java
@@ -1,14 +1,26 @@
 package com.acn.componenthealthmonitor;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.bluetooth.BluetoothDevice;
+import android.content.Context;
 import android.content.Intent;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
 
 import no.nordicsemi.android.thingylib.BaseThingyService;
 import no.nordicsemi.android.thingylib.ThingyConnection;
 
+import static no.nordicsemi.android.dfu.DfuBaseService.NOTIFICATION_ID;
+
 public class ThingyService extends BaseThingyService {
+
+    private static final String PRIMARY_CHANNEL = "Thingy:52 Connectivity Status";
+    private static final String PRIMARY_CHANNEL_ID = "com.acn.componenthealthmonitor.connectivity.status";
+    private NotificationChannel mNotificationChannel;
 
     public class ThingyBinder extends BaseThingyBinder {
         @Override
@@ -17,9 +29,63 @@ public class ThingyService extends BaseThingyService {
         }
     }
 
+    @Override
+    public void onDeviceConnected(final BluetoothDevice device, final int connectionState) {
+    }
+
+    @Override
+    public void onDeviceDisconnected(final BluetoothDevice device, final int connectionState) {
+        super.onDeviceDisconnected(device, connectionState);
+        cancelNotification(device);
+    }
+
     @Nullable
     @Override
     public ThingyBinder onBind(final Intent intent) {
         return new ThingyBinder();
     }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        createNotificationPrerequisites();
+        startForeground(NOTIFICATION_ID, createForegroundNotification());
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void createNotificationPrerequisites() {
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (mNotificationChannel == null) {
+            mNotificationChannel = new NotificationChannel(PRIMARY_CHANNEL_ID, PRIMARY_CHANNEL, NotificationManager.IMPORTANCE_LOW);
+        }
+        mNotificationManager.createNotificationChannel(mNotificationChannel);
+    }
+
+    private Notification createForegroundNotification() {
+        final NotificationCompat.Builder builder = getBackgroundNotificationBuilder();
+        builder.setColor(ContextCompat.getColor(getApplicationContext(), R.color.colorAccent));
+        builder.setContentTitle(("Bluetooth car is running."));
+
+        return builder.build();
+    }
+
+    private NotificationCompat.Builder getBackgroundNotificationBuilder() {
+        final Intent parentIntent = new Intent(this, MainActivity.class);
+        parentIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        final NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), PRIMARY_CHANNEL);
+        builder.setSmallIcon(R.drawable.ic_car_white);
+        builder.setChannelId(PRIMARY_CHANNEL_ID);
+        return builder;
+    }
+
+    private void cancelNotification(final BluetoothDevice device) {
+        final NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        nm.cancel(device.getAddress(), NOTIFICATION_ID);
+    }
 }
+
